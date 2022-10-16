8aa87a62d0070e872bd749c1dc3bb3a6a4f81267
RemyXVX <remyfranciscoflores@gmail.com>
2022-10-16 15:20:04 -0700

    Add our api into our BL

--- __test__/bike.test.js
+++ __test__/bike.test.js
@@ -1,5 +1,11 @@
+import { TestScheduler } from 'jest';
 import Bike from '../src/bike'
 
 describe('Bike', ()=> {
+  let bikes;
+  beforeEach(()=> {
+    bikes = new Bike()
+  });
   
+  test("should ")
 })
--- src/bike.js
+++ src/bike.js
@@ -1,3 +1,16 @@
 export default class Bike {
-  
+  static bikes(location, color) {
+    return fetch ('https://bikeindex.org:443/api/v3/search?page=1&per_page=5&colors=all&location=all&distance=10&stolenness=all')
+    .then(function(response) {
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}`;
+        throw new Error(errorMessage);
+      } else {
+        return response.json();
+      }
+    })
+    .catch(function(error) {
+      return error;
+    });
+  }
 }
