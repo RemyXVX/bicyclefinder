eac510619932808266eef9d0376075c6448ec6e4
RemyXVX <remyfranciscoflores@gmail.com>
2022-10-18 20:03:42 -0700

    updating business and UI
    Co-authored-by: Oscar Gonzalez <ogon318@gmail.com>

--- src/bike.js
+++ src/bike.js
@@ -1,5 +1,5 @@
 export default class Bike {
-  static bikes(location, color) {
+  static bikes(location) {
     return fetch ('https://bikeindex.org:443/api/v3/search?page=1&per_page=5&colors=all&location=all&distance=10&stolenness=all')
       .then(function(response) {
         if (!response.ok) {
--- src/index.js
+++ src/index.js
@@ -2,4 +2,34 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-//import Bike from './bike';
+import Bike from './bike.js';
+// Business Logic
+function bikes(location) {
+  Bike.bikes(location)
+    .then(function(response) {
+      if(response.main) {
+        printElements(response, location);
+      } else {
+        printError(response, location);
+      }
+  });
+}
+//UI Logic
+function printElements(response, location) {
+  document.querySelector('#pushStuffHere').innerHTML = `Your bike got jacked in ${location}. Look for it there.`;
+}
+
+function printError(error, location) {
+  document.querySelector('#pushStuffHere').innerHTML = `There was an error getting the ${location}. Sorry ese.`;
+}
+
+function handleFormSubmission(event)  {
+  event.preventDefault();
+  const location = document.querySelector('#city').value;
+  document.querySelector('#city').value = null;
+  bikes(location);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form#bikes').addEventListener("submit", handleFormSubmission);
+});
